0a722b23e23a1f0243819a803dffae0c9e7e35ce
end5 <16655171+end5@users.noreply.github.com>
2018-11-13 14:53:56 -0800

    Implemented and cleaned up time aware code.

---
+++ Game/ITimeEvent.ts
@@ -0,0 +1,11 @@
+import { Character } from "./Character/Character";
+import { NextScreenChoices } from "./ScreenDisplay";
+
+export interface ITimeEvent {
+    /**
+     * Called once per hour. If NextScreenChoices is returned, the loop for updating is broken
+     * and will resume when TimeEvents.update() is called.
+     * @param player
+     */
+    update(player: Character): void | NextScreenChoices;
+}
--- Game/IUpdate.ts
+++
@@ -1,4 +0,0 @@
-// Used for game updates
-export interface IUpdate {
-    update(hours: number): void;
-}
