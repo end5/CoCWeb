2614399f7cf996f80f698e83dff26fbb876320fb
end5 <16655171+end5@users.noreply.github.com>
2018-11-21 18:10:29 -0800

    Changed Flags to revert to default value on clear.

--- Game/Flags.ts
+++ Game/Flags.ts
@@ -2,10 +2,33 @@ import { FlagType } from './FlagType';
 import { Dictionary } from '../Engine/Utilities/Dictionary';
 
 class FlagDict extends Dictionary<FlagType, object> {
+    private baseValues: IDictionary<object> = {};
+
+    /**
+     * Gets the entry at the key. If no entry is found, an empty object is returned.
+     * @param key
+     */
     public get<F extends object>(key: FlagType): F {
         if (!this.has(key))
             this.dictionary[key] = {};
         return super.get(key) as F;
+    }
+
+    /**
+     * Sets the key to entry. This is the default entry for the key.
+     * @param key
+     * @param entry
+     */
+    public set(key: FlagType, entry: object) {
+        this.baseValues[key] = entry;
+        super.set(key, JSON.parse(JSON.stringify(entry)));
+    }
+
+    /**
+     * Clears the dictionary and restores the default values.
+     */
+    public clear() {
+        this.dictionary = JSON.parse(JSON.stringify(this.baseValues));
     }
 }
 
