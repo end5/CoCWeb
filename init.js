518ea80e6a070514ec43f5e574e0bab0a94cc9e3
end5 <16655171+end5@users.noreply.github.com>
2019-06-13 17:22:25 -0700

    Add file saver to init script

--- init.js
+++ init.js
@@ -4,3 +4,5 @@ if (!fs.existsSync('Build'))
     fs.mkdirSync('Build');
 
 fs.copyFileSync('node_modules/requirejs/require.js', 'Build/require.js');
+
+fs.copyFileSync('node_modules/file-saver/dist/FileSaver.min.js', 'Build/FileSaver.min.js');
