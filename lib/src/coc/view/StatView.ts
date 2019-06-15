e98cc5a4211e768c1e666f3646bfea33fd30b202
end5 <16655171+end5@users.noreply.github.com>
2019-06-15 15:41:50 -0700

    Fix stats not rounding

--- lib/src/coc/view/StatView.ts
+++ lib/src/coc/view/StatView.ts
@@ -34,7 +34,7 @@ export class StatView {
     }
 
     public setNumber(num: number) {
-        this.currNumber.textContent = num + '';
+        this.currNumber.textContent = Math.round(num) + '';
     }
 
     public showUp() {
